9603df91b7cd5ae07e7201f329d49ec4b8425421
i-qbu <iqbu.lab@gmail.com>
2023-12-31 06:33:51 +0900

    Initial Commit

---
+++ Myfirst/MyfirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstApp.swift
+//  Myfirst
+//
+//  Created by 渡辺大智 on 2023/12/31.
+//
+
+import SwiftUI
+
+@main
+struct MyfirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
